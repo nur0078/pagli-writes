45b1258a056ec419ac13253d4ce9a2227c6520b1
Anurodh <anurodhp7@gmail.com>
2023-06-12 02:00:26 +1000

    indexed components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,12 @@
 export { default as LandingPage } from './LandingPage';
 
 export { default as AboutPage } from './About';
+
+export { default as GalleryPage } from './Gallery';
+
+export { default as GhazalsPage } from './Ghazals';
+
+export { default as PoemsPage  } from './Poems';
+
+ 
+
