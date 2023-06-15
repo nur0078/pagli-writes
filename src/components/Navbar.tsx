84b87afa7a7f1b50800e689812b19063849a6cf5
Anurodh <anurodhp7@gmail.com>
2023-06-16 03:55:09 +1000

    navbar completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h4 className="logo--text"> RUKMINI</h4>
-      <div className="menu--list">
-        <ul className="menu--items">
-          <li className="menu-category">Home</li>
-          <li className="menu-category">Poems</li>
-          <li className="menu-category">Ghazals</li>
-          <li className="menu-category">Gallery</li>
-          <li className="menu-category">About</li>
-        </ul>
+    <nav className=" shadow-md space-x-4">
+      <div className="flex justify-between text-center py-4 px-2">
+        <a href="/" className="text-2xl font-semibold no-underline  flex  px-2 hover:text-pencil ">
+          RUKMINI
+        </a>
+        <div className=" font-inter flex items-center space-x-4">
+          <a href="/" className="text-lg no-underline hover:text-pencil">
+            {' '}
+            Home
+          </a>
+          <a href="/poems" className="text-lg no-underline hover:text-pencil">
+            Poems
+          </a>
+          <a href="/ghazals" className="text-lg no-underline hover:text-pencil">
+            Ghazals
+          </a>
+          <a href="/gallery" className="text-lg no-underline hover:text-pencil">
+            Gallery
+          </a>
+          <a href="/about" className="text-lg no-underline hover:text-pencil">
+            About
+          </a>
+        </div>
       </div>
     </nav>
   );
