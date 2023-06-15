639f1ab1034ea95d64e0c769ed83b343ee78034c
Anurodh <anurodhp7@gmail.com>
2023-06-16 04:49:01 +1000

    cleaned up footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className=" items-center text-center text-lg py-4  ">
+    <footer className=" text-center text-lg mt-auto   ">
       <h4> Copyright &copy; 2023. Rukmini 'Pagli'. </h4>
     </footer>
   );
