cce8b73fcba0640450a61d348729bfaf3c251055
Anurodh <anurodhp7@gmail.com>
2023-06-17 06:03:42 +1000

    added card component to display articles in '/'

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className=" h-48 opacity-50 shadow-2xl rounded-lg px-3 border-pencil border-t-4">
+      <h3 className="text-xl font-bold text-center py-2"> Gajal 1 </h3>
+      <article className="text-sm py-8">
+        Cupidatat non minim eu esse sunt ad voluptate deserunt. Cupidatat non minim eu esse sunt ad
+        voluptate deserunt.
+      </article>
+    </div>
+  );
+};
+
+export default Card;
