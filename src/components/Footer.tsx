adb8e49d33f74d10ebec57deaf746156650c57ee
Anurodh <anurodhp7@gmail.com>
2023-06-17 06:04:27 +1000

    updated footer and navbar styles as per figma

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className=" text-center text-lg mt-auto   ">
+    <footer className=" text-center text-lg mt-auto pt-28 ">
       <h4> Copyright &copy; 2023. Rukmini 'Pagli'. </h4>
     </footer>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className=" shadow-md space-x-4">
-      <div className="flex justify-between text-center py-4 px-2">
-        <a href="/" className="text-2xl font-bold no-underline  flex  px-2 hover:text-pencil ">
+    <nav className=" shadow-md space-x-4 w-screen">
+      <div className="flex justify-between text-center py-4 px-2 w-11/12">
+        <a href="/" className="text-2xl font-bold no-underline flex  px-6 hover:text-pencil ">
           RUKMINI
         </a>
         <div className=" font-inter flex items-center space-x-4">
