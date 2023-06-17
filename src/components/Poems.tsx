8c2198b2ea2b0c6f71456561bf2f84ae33c75a87
Anurodh <anurodhp7@gmail.com>
2023-06-17 17:44:22 +1000

    added PoemPage for /poems

--- src/components/Poems.tsx
+++ src/components/Poems.tsx
@@ -1,36 +1,32 @@
 import React from 'react';
+import Card from './Card';
 import Navbar from './Navbar';
-import RukuPic from '../assets/images/PoemPage/blackDress.png';
 import Footer from './Footer';
 
-const Poems = () => {
+const Poem = () => {
   return (
-    <div>
+    <div className="min-h-screen ">
       <Navbar />
-      <div className="min-h-screen relative">
-        <div className="pt-20 grid grid-cols-12 gap-4 pb-8">
-          <h3 className=" text-3xl col-start-6 col-span-1 pt-14 "> Poems </h3>
-          <article className="col-start-2 col-span-5 justify-content text-pencil text-xl pt-10">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </article>
-          <img
-            className="col-start-8 col-span-4 h-96 rounded-2xl mt-10 "
-            src={RukuPic}
-            alt="Ruku looking pretty"
-          />
-        </div>
+      <h3 className="text-6xl font-bold text-center pt-10"> Poems </h3>
 
-        <div className="pt-8 aboslute bottom-0 ">
-          <Footer />
+      {/* DISPLAY GRIDS */}
+      <div className="pt-20 grid grid-cols-12 gap-4 w-11/12">
+        <div className=" col-start-2 col-span-4 gap-4 justify-around ">
+          <Card />
+        </div>
+        <div className=" col-start-8 col-span-4 gap-4 justify-around ">
+          <Card />
+        </div>
+        <div className=" col-start-2 col-span-4 gap-4 justify-around pt-20">
+          <Card />
+        </div>
+        <div className=" col-start-8 col-span-4 gap-4 justify-around pt-20 ">
+          <Card />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
 
-export default Poems;
+export default Poem;
