c9b0fcb68b017828b00a580717c7129bed35302f
Anurodh <anurodhp7@gmail.com>
2023-06-17 17:46:29 +1000

    made gallery page for /gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const Gallery = () => {
-    return <div>Gallery</div>;
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Selfie from '../assets/images/GalleryPage/selfie.jpeg';
+
+const Poem = () => {
+  return (
+    <div className="min-h-screen ">
+      <Navbar />
+      <h3 className="text-6xl font-bold text-center pt-10"> GALLERY </h3>
+
+      {/* DISPLAY GRIDS */}
+      <div className=" pt-20 grid grid-cols-12 gap-8  w-11/12">
+        <div className=" col-start-3 col-span-4 gap-4 justify-around ">
+          <img src={Selfie} alt="Ruku" className=" rounded-xl shadow-2xl " />
+        </div>
+        <div className=" col-start-8 col-span-4 gap-4 justify-around ">
+          <img src={Selfie} alt="Ruku" className="rounded-xl shadow-2xl" />
+        </div>
+        <div className=" col-start-3 col-span-4 gap-4 justify-around pt-6 ">
+          <img src={Selfie} alt="Ruku" className="rounded-xl shadow-2xl" />
+        </div>
+        <div className=" col-start-8 col-span-4 gap-4 justify-around pt-6 ">
+          <img src={Selfie} alt="Ruku" className="rounded-xl shadow-2xl " />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
-export default Gallery;
+export default Poem;
