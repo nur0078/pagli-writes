b49c625285ef548f490841a934fb3090671aea48
sital999 <sitalnagarkoti123@gmail.com>
2023-06-20 09:36:21 +0545

    extend color along with default props

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         caveat: ['Caveat', 'cursive'],
         inter: ['Inter', 'sans-serif'],
       },
-    },
-    colors: {
-      primary: "#DFDCCF",     // background color
-      main: "#000000",
-      pencil: "#46535B",
-      btn: "#AE9E9E", // background color for (read more..) button
-
+      colors: {
+        primary: '#DFDCCF', // background color
+        main: '#000000',
+        pencil: '#46535B',
+        btn: '#AE9E9E', // background color for (read more..) button
+      },
     },
   },
   plugins: [],
-}
+};
