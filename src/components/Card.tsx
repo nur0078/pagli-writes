8d113b664a180942ba6a080311c9e30ba4eaeb60
sital999 <sitalnagarkoti123@gmail.com>
2023-06-20 11:01:52 +0545

    make card dynamic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,15 @@
-const Card = () => {
+interface ICardProps {
+  poem: { title: string; content: string };
+}
+
+const Card = ({ poem }: ICardProps) => {
   return (
-    <div className=" h-48 opacity-50 shadow-2xl rounded-lg px-3 border-pencil border-t-4">
-      <h3 className="text-xl font-bold text-center py-2"> Ghazal 1 </h3>
-      <article className="text-sm py-8">
-        Cupidatat non minim eu esse sunt ad voluptate deserunt.
-      </article>
+    <div className=" h-60 opacity-50 shadow-2xl text-center space-y-4 rounded-lg p-2 border-pencil border-t-4 w-80 truncate">
+      <h3 className="text-xl font-bold text-center py-2"> {poem.title} </h3>
+      <article
+        dangerouslySetInnerHTML={{ __html: poem.content }}
+        className="text-ellipsis overflow-hidden"
+      />
     </div>
   );
 };
